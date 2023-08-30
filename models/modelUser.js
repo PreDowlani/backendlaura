006b7fefd2f3748fd122786bf91c2150be70bdae
Prashanth Dowlani <prashanth.work01@gmail.com>
2023-08-30 21:52:13 +0200

    intentando arreglar el maldito contraseña

--- models/modelUser.js
+++ models/modelUser.js
@@ -2,25 +2,52 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 // Definir el esquema del usuario
-const usuarioSchema = new mongoose.Schema({
-  nombreCompleto: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    match: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+// const usuarioSchema = new mongoose.Schema({
+//   nombreCompleto: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 8,
+//     match: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
     // El regex asegura que la contraseña contenga al menos una mayúscula, un dígito y un carácter especial
-  }
+//   }
+// });
+
+
+
+const usuarioSchema = new mongoose.Schema({
+    nombreCompleto: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function(value) {
+            // const contraseñaRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+            const contraseñaRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&]).{8,}$/;
+
+            return contraseñaRegex.test(value);
+          },
+          message: props => `La contraseña no cumple con los requisitos.`
+      }
+    }
 });
 
 // Aplicar el plugin de mongoose-unique-validator al esquema
@@ -30,4 +57,3 @@ usuarioSchema.plugin(uniqueValidator);
  
 
 module.exports = new mongoose.model('Usuario', usuarioSchema);
-
